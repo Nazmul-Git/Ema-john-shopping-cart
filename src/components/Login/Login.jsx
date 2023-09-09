3a36e16dec11b58458182d0d1ae075a779a2bb12
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-10 01:20:19 +0600

    Implement show password in login component & set up firebase deploy.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,87 +1,97 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../AuthProvider";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import image from '../../images/bg-img.jpg'
 
 const Login = () => {
-    const {logInUser,googleSignIn}=useContext(AuthContext);
-    let navigate=useNavigate();
-    const location=useLocation();
-    // console.log(location);
+    const [showPass, setShowPass] = useState(false);
 
-    const from=location.state?.from?.pathname || '/';
-    // console.log('from=', from);
+    const { logInUser, googleSignIn } = useContext(AuthContext);
 
-    const loggedUserDetail=(e)=>{
+    let navigate = useNavigate();
+    const location = useLocation();
+    console.log(location);
+
+    const from = location.state?.from?.pathname || '/';
+    console.log('from=', from);
+
+    const loggedUserDetail = (e) => {
         e.preventDefault();
-        const form= e.target;
-        const email= form.email.value;
-        const pass= form.password.value;
-        // console.log(email, pass);
+        const form = e.target;
+        const email = form.email.value;
+        const pass = form.password.value;
+        console.log(email, pass);
 
         logInUser(email, pass)
-        .then(result=>{
-            const loggedUser= result.user;
-            console.log(loggedUser);
-            form.reset();
-            navigate(from, { replace : true });
-        })
-        .catch(error=>{
-            console.error(error);
-        })
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset();
+                navigate(from, { replace: true });
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 
-    const handleGoogleSignIn=()=>{
+    const handleGoogleSignIn = () => {
         googleSignIn()
-        .then(result=>{
-            const loggedUser=result.user;
-            // console.log(loggedUser);
-        })
-        .catch(error=>{
-            console.error(error.message);
-        })
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+            })
+            .catch(error => {
+                console.error(error.message);
+            })
     }
     return (
         <div>
-            <img src={image} alt="" className="relative"/>
-            
-            
-                <div className="hero min-h-screen bg-base-200">
-                    <div className="hero-content flex-col absolute ">
-                        <div className="text-center">
-                            <h1 className="text-5xl font-bold text-blue-600">Login now!</h1>
-                        </div>
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <form onSubmit={loggedUserDetail} className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input type="email" name='email' required placeholder="email" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input type="password" name='password' required placeholder="password" className="input input-bordered" />
-                                    <label className="label">
-                                        <Link to='/sign-up' className="label-text-alt link link-hover">Forgot password?</Link>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="btn btn-primary">Login</button>
-                                </div>
-                            </form>
-                            <Link to='/' className="text-center"> 
-                               <button onClick={handleGoogleSignIn} className="btn btn-xs m-6 hover:text-blue-600">Sign in with google.</button>
-                                      
-                            </Link>
+            <img src={image} alt="" className="relative" />
 
-                            
-                        </div>
+
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col absolute ">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold text-blue-600">Login now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={loggedUserDetail} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' required placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type={showPass? "text":"password"} name='password' required placeholder="password" className="input input-bordered" />
+                                <div onClick={() => setShowPass(!showPass)}>
+                                    <span className=" font-thin text-sm ml-32">
+                                        {
+                                            showPass ? <span>Hide Password</span> : <span>Show Password</span>
+                                        }
+                                    </span>
+                                </div>
+                                <label className="label">
+                                    <Link to='/sign-up' className="label-text-alt link link-hover">Forgot password?</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                        <Link to='/' className="text-center">
+                            <button onClick={handleGoogleSignIn} className="btn btn-xs m-6 hover:text-blue-600">Sign in with google.</button>
+
+                        </Link>
+
+
                     </div>
                 </div>
-            
+            </div>
+
 
             {/* {
                 user && <div className=' flex flex-1 justify-center items-center gap-4 mt-28'>
